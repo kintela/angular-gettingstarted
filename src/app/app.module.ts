09bedb7814f3c9a6b7ade2b52439e3fa886fcba6
Kintela <roberto.quintela@gmail.com>
2019-09-16 18:42:44 +0200

    Intercambio entre componentes anidados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
- import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
+import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
@@ -10,7 +11,8 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
