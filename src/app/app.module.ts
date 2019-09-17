b326e7200a63b8b7814afa8c0da03c495e531630
Kintela <roberto.quintela@gmail.com>
2019-09-17 12:58:16 +0200

    usar HTTP con observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
@@ -16,7 +17,8 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,42 +1,35 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn:'root'
 })
 export class ProductService{
-   getProducts():IProduct[]{
-     return [
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2019",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId":2,
-        "productName":"Garden Cart",
-        "productCode":"GDN-0023",
-        "releaseDate":"March 18, 2019",
-        "description":"15 gallon capacity rolling garden",
-        "price":32.99,
-        "starRating":4.2,
-        "imageUrl":"assets/images/garden_cart.png"
-      },
-      {
-        "productId":5,
-        "productName":"Hammer",
-        "productCode":"TBX-0048",
-        "releaseDate":"May 21, 2019",
-        "description":"Curved claw steel hammer",
-        "price":8.9,
-        "starRating":4.8,
-        "imageUrl":"assets/images/hammer.png"
-      }
-    ];
-   }
+  private productUrl='api/products/products.json';
+
+  constructor(private http:HttpClient){}
+
+  getProducts():Observable<IProduct[]>{
+     return this.http.get<IProduct[]>(this.productUrl).pipe(
+       tap(data=>console.log('All: ' + JSON.stringify(data))),
+       catchError(this.handleError)
+     );   
+  }
+
+  private handleError(err:HttpErrorResponse){
+    let errorMessage='';
+    if (err.error instanceof ErrorEvent) {
+      //Ha sido un error en el lado del cliente o es un problema de red
+      errorMessage=`An error ocurred: ${err.error.message}`;
+    }else{
+      //El back-end ha develto un código de respuesta no valido
+      //El cuerpo de la respuesta puede contener algo raro
+      errorMessage=`Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
